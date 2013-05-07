0a8a8dbaf51946cd9fc7146fb57c6fec1cd45efd
camille <camille.thomassin@intech.lu>
2013-05-07 11:24:20 +0200

    snap 0.0.5

--- src/main/java/servlets/Test.java
+++ src/main/java/servlets/Test.java
@@ -12,6 +12,7 @@ public class Test extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
 		String message = "My awesome message3";
 		req.setAttribute("test", message);
 		this.getServletContext().getRequestDispatcher( "/WEB-INF/jsp/test.jsp" ).forward( req, resp );
